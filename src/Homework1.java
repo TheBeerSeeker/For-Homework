6b803aabfda276c0ba2117c568b469353faa9479
TheBeerSeeker <leftmetalhead@gmail.com>
2018-12-25 23:24:09 +0700

    Поправил немношк

--- src/Homework1.java
+++ src/Homework1.java
@@ -17,6 +17,7 @@ public class Homework1 {
         Scanner scan = new Scanner(System.in);
 
         //задание 3
+
         System.out.println("Сейчас я буду производить все вычисления!");
         int a = 100;
         int b = 200;
@@ -25,6 +26,7 @@ public class Homework1 {
         System.out.println(calc(a, b, c, d));
 
         //задание 4
+
         System.out.println("Сейчас узнаем, равняется ли сумма чисел промежутку от 10 до 20!");
         System.out.println("Введите числа: ");
         int st = scan.nextInt();
@@ -32,12 +34,14 @@ public class Homework1 {
         System.out.println(calc1(st, nd));
 
         //задание 5
+
         System.out.println("Сейчас узнаем, положительное число или отрицательное!");
         System.out.println("Введите число: ");
         int x = scan.nextInt();
         porn(x);
 
         //задание 6
+
         System.out.println("Сейчас узнаем, правда ли ваше число отрицательное!");
         System.out.println("Введите число: ");
         int y = scan.nextInt();
@@ -51,18 +55,21 @@ public class Homework1 {
         name(yourname);
 
         //задание 8
+
         System.out.println("Сейчас узнаем, является ли год високосным!");
         System.out.println("Введи год:");
         int year = scan.nextInt();
         visgod(year);
-
         }
+
         //метод для задания 3
+
         public static int calc(int a, int b, int c, int d) {
             return a * (b + (c / d));
         }
 
         //метод для задания 4
+
         public static boolean calc1(int st, int nd) {
             if((st + nd) >= 10 && (st + nd) <= 20) {
                 return true;
@@ -70,6 +77,7 @@ public class Homework1 {
         }
 
         //метод для задания 5
+
         public static void porn(int x) {
             if (x >= 0) {
                 System.out.println("Положительное");
@@ -77,19 +85,21 @@ public class Homework1 {
         }
 
         //метод для задания 6
+
         public static boolean nornot(int y) {
             if (y < 0) {
                 return true;
-            } else {
-                return false;
-                }
-            }
+            } else return false;
+        }
+
         //метод для задания 7
+
         public static void name(String yourname) {
             System.out.println("Привет, " + yourname + "!");
         }
 
         //метод для задания 8
+
         public static void visgod(int year) {
             if ((year % 4 == 0) && (year % 100 != 0)) {
                 System.out.println("Этот год високосный!");
