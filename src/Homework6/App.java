748d2324ab53c345ec6420d70a3bf7e343deac12
TheBeerSeeker <leftmetalhead@gmail.com>
2019-01-29 08:10:27 +0700

    Домашка 6

---
+++ src/Homework6/App.java
@@ -0,0 +1,14 @@
+package Homework6;
+
+public class App {
+
+    public static void main(String[] args) {
+
+    KittyCat kitty = new KittyCat("Мурка", "Сибирская", "Рыжая");
+    kitty.swim(2);
+    kitty.run(1000);
+    Dog doge = new Dog("Шарик", "Мопс", "Серый");
+    doge.jump(2.5);
+    doge.swim(6);
+    }
+}
